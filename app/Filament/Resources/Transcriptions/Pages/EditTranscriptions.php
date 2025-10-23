cbf2004bc7543842fdf9b97cab4390a3d7e2d822
ArmanAmeerAli <154812221+ArmanAmeerAli@users.noreply.github.com>
2025-10-23 23:18:08 +0500

    Add Transcriptions resource with Filament integration

---
+++ app/Filament/Resources/Transcriptions/Pages/EditTranscriptions.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\Transcriptions\Pages;
+
+use App\Filament\Resources\Transcriptions\TranscriptionsResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditTranscriptions extends EditRecord
+{
+    protected static string $resource = TranscriptionsResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            ViewAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+}
